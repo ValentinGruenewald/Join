432293ff91e776b9239935d1e0e5e956a358c145
Valentin Olberding <va.olberding@gmail.com>
2022-06-11 17:48:23 +0200

    test

---
+++ js/add-taskV.js
@@ -0,0 +1,85 @@
+tasks = [
+    {
+        'title': 'Title1',
+        'category': 'Category1',
+        'description': 'This is Task 1',
+        'due-date': '2022-06-10',
+        'urgency': 'High',
+        'assigned-to': 'Marco'
+    }
+]
+
+users = [
+    {
+        'name': 'Marco',
+        'selected': true
+    },
+    {
+        'name': 'Valentin',
+        'selected': false
+    },
+    {
+        'name': 'Tom',
+        'selected': false
+    },
+    {
+        'name': 'Me',
+        'selected': false
+    }
+]
+
+currentUser = 'Marco';
+
+function addTask() {
+    let newTask = {
+        'title': `${document.getElementById('title').value}`,
+        'category': `${document.getElementById('category').value}`,
+        'description': `${document.getElementById('description').value}`,
+        'due-date': `${document.getElementById('due-date').value}`,
+        'urgency': `${document.getElementById('urgency').value}`,
+        'assigned-to': `${currentUser}`
+    };
+    tasks.push(newTask);
+    clearForm();
+    renderTasks();
+}
+
+function renderTasks() {
+    document.getElementById('test-tasks-container').innerHTML = ``;
+    for (let i = 0; i < tasks.length; i++) {
+        document.getElementById('test-tasks-container').innerHTML += `
+    <div class="task">
+    ${tasks[i]['title']}
+    ${tasks[i]['category']}
+    ${tasks[i]['description']}
+    ${tasks[i]['due-date']}
+    ${tasks[i]['urgency']}
+    ${tasks[i]['assigned-to']}
+    </div>
+    `;
+    }
+}
+
+function clearForm() {
+    document.getElementById('title').value = ''
+    document.getElementById('category').value = ''
+    document.getElementById('description').value = ''
+    document.getElementById('due-date').value = ''
+    document.getElementById('urgency').value = 'Low'
+    clearUsers()
+    document.getElementById('user_1').classList.add('selected-user');
+    currentUser = users[0]['name'];
+}
+
+
+function selectUser(user) {
+    clearUsers()
+    document.getElementById(`user_${user}`).classList.add('selected-user');
+    currentUser = users[user-1]['name'];
+}
+
+function clearUsers() {
+    for (let i = 1; i < users.length+1; i++) {
+        document.getElementById(`user_${i}`).classList.remove('selected-user');
+    }
+}
