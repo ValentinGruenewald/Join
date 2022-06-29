96c8f5e6a4bf0d00b6770e6330b6fce6868c5bed
MarcoScherf <scheco13@gmail.com>
2022-06-29 22:30:34 +0200

    add mobile move Task and Board is empty show message

--- js/board.js
+++ js/board.js
@@ -5,20 +5,29 @@ function getID(id) {
 }
 
 function updateHTML() {
+    if (tasks.length == 0) {
+        getID('empty-container').style.display = 'flex';
+        clearColums();
+    } else {
+        clearColums();
+        getID('empty-container').style.display = 'none';
+        let todo = tasks.filter(x => x['place'] == 'todo');
+        renderTask(todo);
+        let inprogress = tasks.filter(x => x['place'] == 'inprogress');
+        renderTask(inprogress);
+        let testing = tasks.filter(x => x['place'] == 'testing');
+        renderTask(testing);
+        let done = tasks.filter(x => x['place'] == 'done');
+        renderTask(done);
+    }
+}
+
+function clearColums() {
     getID('todo').innerHTML = '';
     getID('inprogress').innerHTML = '';
     getID('testing').innerHTML = '';
     getID('done').innerHTML = '';
-    let todo = tasks.filter(x => x['place'] == 'todo');
-    renderTask(todo);
-    let inprogress = tasks.filter(x => x['place'] == 'inprogress');
-    renderTask(inprogress);
-    let testing = tasks.filter(x => x['place'] == 'testing');
-    renderTask(testing);
-    let done = tasks.filter(x => x['place'] == 'done');
-    renderTask(done);
 }
-
 
 function renderTask(task) {
     for (let i = 0; i < task.length; i++) {
@@ -31,15 +40,18 @@ function renderTask(task) {
 
 function templateTasks(task, i) {
     return ` 
-        <div class="board-task" id="${task['place']}${i}" draggable="true" ondragstart="startDragging(${task['id']})" ondragend="removeHover()">
+        <div class="board-task cursor" id="${task['place']}${i}" draggable="true" ondragstart="startDragging(${task['id']})" ontouchmove="moveTo('inprogress')" ondragend="removeHover()">
             <div class="board-category flex-end">${task.category}</div>
             <div class="board-title">${task.title}</div>
             <div class="board-description">${task.description}</div>
             <div class="board-member flex-end">${task['assigned-to']}</div>
             <div class="board-footer">
                 <div class="board-date">${formateDate(task['due-date'])}</div>
-                <div class="cursor"><img onclick="deleteTask(${task['id']})" src="img/basket.png"</div>
+                <div class="hover">
+                    <img onclick="deleteTask(${task['id']})" src="img/basket.png">
+                </div>
             </div>
+            <div class="hover move"><img src="img/finish.png" onclick="nextSection(${task['id']}, '${task['place']}')"></div>
         </div>
     `;
 }
@@ -113,4 +125,24 @@ async function deleteTask(id) {
 function formateDate(dateStr) {
     let dArr = dateStr.split("-");
     return dArr[2] + "." + dArr[1] + "." + dArr[0];
+}
+
+
+function nextSection(id, place) {
+    let taskPosition = tasks.findIndex(obj => obj.id == id);
+    switch (place) {
+        case 'todo':
+            tasks[taskPosition].place = 'inprogress';
+            break;
+        case 'inprogress':
+            tasks[taskPosition].place = 'testing';
+            break;
+        case 'testing':
+            tasks[taskPosition].place = 'done';
+            break;
+        case 'done':
+            tasks[taskPosition].place = 'todo';
+            break;
+    }
+    save()
 }
